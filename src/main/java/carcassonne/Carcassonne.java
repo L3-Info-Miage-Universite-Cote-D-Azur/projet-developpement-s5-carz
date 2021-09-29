bf1155fcd21ec02f460acfe8e6f49e4695421910
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2021-09-29 09:32:19 +0200

    this closes #8 . Le nombre de joueur est limité. Ajout d'un document sur le découpage

--- src/main/java/carcassonne/Carcassonne.java
+++ src/main/java/carcassonne/Carcassonne.java
@@ -4,7 +4,12 @@ import carcassonne.joueur.Joueur;
 
 import java.util.ArrayList;
 
+/**
+ * Classe qui gère le déroulement du jeu
+ * Point d'entrée pour l'exécution du projet
+ */
 public class Carcassonne {
+    public static final int NB_MAX_JOUEURS = 5;
 
     boolean fini ;
     private ArrayList<Joueur> listeDeJoueurs;
@@ -24,15 +29,28 @@ public class Carcassonne {
     }
 
 
-
+    /**
+     * exécution de la partie : faire les tours jusqu'à la fin du jeu
+     */
     public void seDérouler() {
         if (listeDeJoueurs.size() > 0) {
             setFini(true);
         }
     }
 
-    public void addJoueur(Joueur joueur) {
-        this.listeDeJoueurs.add(joueur);
+    /**
+     * pour ajouter un joueur à la liste des joueurs, s'il y a insuffisamment de joueurs
+     * @param joueur
+     * @return true si l'ajout est effectif, faux s'il y a trop de joueur
+     */
+    public boolean addJoueur(Joueur joueur) {
+        if (this.listeDeJoueurs.size() >= NB_MAX_JOUEURS) {
+            return false;
+        }
+        else {
+            this.listeDeJoueurs.add(joueur);
+            return true;
+        }
     }
 
     public static final void main(String [] args) {
--- src/test/java/carcassonne/CarcassonneTest.java
+++ src/test/java/carcassonne/CarcassonneTest.java
@@ -25,4 +25,14 @@ class CarcassonneTest {
         assertTrue(objetDuTest.getFini());
     }
 
+    @Test
+    public void ajoutDeJoueur(){
+        Joueur [] joueurs = new Joueur[10];
+        for(int i = 0; i < joueurs.length; i++) {
+            joueurs[i] = new Joueur("test "+i);
+            boolean resultatAjout = objetDuTest.addJoueur(joueurs[i]);
+            if (i < Carcassonne.NB_MAX_JOUEURS) assertTrue(resultatAjout, "ajout du "+i+"e joueur aurait du se faire");
+            else assertFalse(resultatAjout, "ajout du "+i+"e joueur aurait du ne pas se faire");
+        }
+    }
 }
